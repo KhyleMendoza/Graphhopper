2d21ddf5c87259f4cac970e0af33f1774eadc3ca
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2024-12-09 14:26:31 +0800

    first commit

---
+++ graphhopper.py
@@ -0,0 +1,9 @@
+import requests
+import urllib.parse 
+
+geocode_url = "https://graphhopper.com/api/1/geocode?"
+route_url = "https://graphhopper.com/api/1/route?"
+loc1 = "Washington, D.C."
+loc2 = "Baltimore, Maryland"
+key = "3e362481-1590-4c04-a78c-e46fa8b12184" 
+
