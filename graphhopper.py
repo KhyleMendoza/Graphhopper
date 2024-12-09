17f615b38277412252e8cca036ac45a479f3ad5b
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2024-12-09 14:57:58 +0800

    v5

--- graphhopper.py
+++ graphhopper.py
@@ -63,5 +63,24 @@ if json_status == 200:
         if loc2 == "quit" or loc2 == "q":
             break
         dest = geocoding(loc2, key)
-        print(dest) 
+        print("=================================================")
+        if orig[0] == 200 and dest[0] == 200: 
+            op="&point="+str(orig[1])+"%2C"+str(orig[2])
+            dp="&point="+str(dest[1])+"%2C"+str(dest[2])
+            paths_url = route_url + urllib.parse.urlencode({"key":key}) + op + dp
+            paths_status = requests.get(paths_url).status_code
+            paths_data = requests.get(paths_url).json()
+            print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url)
+            print("=================================================")
+            print("Directions from " + orig[3] + " to " + dest[3])
+            print("=================================================")
+            if paths_status == 200:
+                miles = (paths_data["paths"][0]["distance"])/1000/1.61
+                km = (paths_data["paths"][0]["distance"])/1000 
+                sec = int(paths_data["paths"][0]["time"]/1000%60)
+                min = int(paths_data["paths"][0]["time"]/1000/60%60)
+                hr = int(paths_data["paths"][0]["time"]/1000/60/60) 
+                print("Distance Traveled: {0:.1f} miles / {1:.1f} km".format(miles, km)) 
+                print("Trip Duration: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
+                print("=================================================")
 
