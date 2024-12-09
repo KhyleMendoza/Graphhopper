bcd3c6300f33853819ba3e756bd8968fe082587b
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2024-12-09 14:43:27 +0800

    v2

--- graphhopper.py
+++ graphhopper.py
@@ -3,7 +3,7 @@ import urllib.parse
 
 geocode_url = "https://graphhopper.com/api/1/geocode?"
 route_url = "https://graphhopper.com/api/1/route?"
-loc1 = "Rome, Italy" 
+loc1 = "Washington, D.C."
 loc2 = "Baltimore, Maryland"
 key = "3e362481-1590-4c04-a78c-e46fa8b12184" 
 
@@ -12,4 +12,44 @@ url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
 replydata = requests.get(url)
 json_data = replydata.json()
 json_status = replydata.status_code
-print(json_data) 
+
+if json_status == 200:
+    def geocoding (location, key):
+        geocode_url = "https://graphhopper.com/api/1/geocode?"
+        url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1",
+        "key":key})
+        replydata = requests.get(url)
+        json_data = replydata.json()
+        json_status = replydata.status_code
+        if json_status == 200:
+            json_data = requests.get(url).json()
+            lat=(json_data["hits"][0]["point"]["lat"])
+            lng=(json_data["hits"][0]["point"]["lng"])
+            name = json_data["hits"][0]["name"]
+            value = json_data["hits"][0]["osm_value"] 
+            if "country" in json_data["hits"][0]:
+                country = json_data["hits"][0]["country"]
+            else:
+                country=""
+
+            if "state" in json_data["hits"][0]:
+                state = json_data["hits"][0]["state"]
+            else:
+                state=""
+
+            if len(state) !=0 and len(country) !=0:
+                new_loc = name + ", " + state + ", " + country
+            elif len(state) !=0: 
+                new_loc = name + ", " + country
+            else:
+                new_loc = name 
+            print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n"+ url) 
+        else:
+            lat="null"
+            lng="null"
+            new_loc=location
+        return json_status,lat,lng, new_loc
+    orig = geocoding(loc1, key)
+    print(orig)
+    dest = geocoding(loc2, key)
+    print(dest) 
