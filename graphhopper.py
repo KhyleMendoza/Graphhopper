28285c0b1c2de201a60056677b4b6f73c81615f7
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2024-12-09 15:01:31 +0800

    v7

--- graphhopper.py
+++ graphhopper.py
@@ -83,4 +83,13 @@ if json_status == 200:
                 print("Distance Traveled: {0:.1f} miles / {1:.1f} km".format(miles, km)) 
                 print("Trip Duration: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
                 print("=================================================")
+                for each in range(len(paths_data["paths"][0]["instructions"])):
+                    path = paths_data["paths"][0]["instructions"][each]["text"]
+                    distance = paths_data["paths"][0]["instructions"][each]["distance"]
+                    print("{0} ( {1:.1f} km / {2:.1f} miles )".format(path, distance/1000,
+                    distance/1000/1.61))
+                    print("=============================================")
+            else:
+                print("Error message: " + paths_data["message"])
+                print("*************************************************") 
 
