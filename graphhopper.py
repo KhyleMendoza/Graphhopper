f2fbe24a51d42bd4be70509029abdef4d343064e
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2024-12-09 14:33:16 +0800

    v1

--- graphhopper.py
+++ graphhopper.py
@@ -3,7 +3,13 @@ import urllib.parse
 
 geocode_url = "https://graphhopper.com/api/1/geocode?"
 route_url = "https://graphhopper.com/api/1/route?"
-loc1 = "Washington, D.C."
+loc1 = "Rome, Italy" 
 loc2 = "Baltimore, Maryland"
 key = "3e362481-1590-4c04-a78c-e46fa8b12184" 
 
+url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key}) 
+
+replydata = requests.get(url)
+json_data = replydata.json()
+json_status = replydata.status_code
+print(json_data) 
